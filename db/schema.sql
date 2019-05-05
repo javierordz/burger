63bb4e2d6a50f8cc7a625e3c0c93c2052c6e9a3d
javierordz <javier.ordz@gmail.com>
2019-05-05 18:06:28 -0500

    structure of folders, basic commands

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Freshman 15', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Kevin Bacon', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Goat', true);
--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var connection = mysql.createConnection({
   port: 3306,
   user: "root",
   password: "bsc0BSC0",
-  database: "burger_db"
+  database: "burgers_db"
 });
 
 connection.connect(function(err) {
