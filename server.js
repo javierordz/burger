94edbea40e4e731103e597d920688d6bd028bba7
javierordz <javier.ordz@gmail.com>
2019-05-05 17:03:38 -0500

    initial push, basic structures

---
+++ server.js
@@ -0,0 +1,56 @@
+var express = require("express");
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "bsc0BSC0",
+  database: "burger_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+app.get("/", function(req, res) {
+    connection.query("SELECT * FROM burgers", function(err, data) {
+        if (err) {
+            return res.status(500).end();
+        }
+        res.render("index", { burgers: data });
+    })
+})
+
+app.post("/burgers", function(req, res) {
+    connection.query("INSERT INTO burgers (burger_name, devoured) VALUES (?, false)", [req.body.burger_name], function(err, result) {
+      if (err) {
+        return res.status(500).end();
+      }
+  
+      // Send back the ID of the new todo
+      res.json({ id: result.insertId });
+      console.log({ id: result.insertId });
+    });
+  });
+
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });  
