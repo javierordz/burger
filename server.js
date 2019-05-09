b18d512cc44fec27b4e48d60aa1690623b64da4a
javierordz <javier.ordz@gmail.com>
2019-05-09 16:23:14 -0500

    cleaned up server.js to leave only Express, HB, and routes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// EXPRESS
 var express = require("express");
 var app = express();
 
@@ -5,52 +6,18 @@ var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 var exphbs = require("express-handlebars");
 
+// HANDLEBARS
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "bsc0BSC0",
-  database: "burgers_db"
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
-app.get("/", function(req, res) {
-    connection.query("SELECT * FROM burgers", function(err, data) {
-        if (err) {
-            return res.status(500).end();
-        }
-        res.render("index", { burgers: data });
-    })
-})
-
-app.post("/burgers", function(req, res) {
-    connection.query("INSERT INTO burgers (burger_name, devoured) VALUES (?, false)", [req.body.burger_name], function(err, result) {
-      if (err) {
-        return res.status(500).end();
-      }
-  
-      // Send back the ID of the new todo
-      res.json({ id: result.insertId });
-      console.log({ id: result.insertId });
-    });
-  });
+// ROUTES
+var routes = require("./controllers/burgers_controller");
+app.use(routes);
 
 app.listen(PORT, function() {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
-  });  
+  console.log("Server listening on: http://localhost:" + PORT);
+});
