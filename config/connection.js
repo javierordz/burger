b1aa04aea2fa22933ce0ff00da3ff4c41e11c33e
javierordz <javier.ordz@gmail.com>
2019-05-09 16:44:12 -0500

    burgers_controller - created router & methods

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,20 @@
-// Inside the `connection.js` file, setup the code to connect Node to MySQL.
+// MYSQL
+var mysql = require("mysql");
 
-//  Export the connection.
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "bsc0BSC0",
+  database: "burgers_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
