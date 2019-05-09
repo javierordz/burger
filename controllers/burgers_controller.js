251b17a2840264e5cc500b73267c456b28961fc9
javierordz <javier.ordz@gmail.com>
2019-05-09 16:40:38 -0500

    burgers_controller - created router & methods

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,4 +1,53 @@
 var express = require("express");
+var router = express.Router();
+
+// MODEL
 var burger = require("../models/burger")
 
-// Create the `router` for the app, and export the `router` at the end of your file.
+// ROUTES
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        var hbsObject = {cats: data}
+        console.log(hbsObject);
+        res.render("index", { burgers: data });
+    })
+});
+
+router.post("/api/burgers", function(req, res) {
+    burger.create(
+        ["name", "sleepy"],
+        [req.body.name, req.body.sleepy],
+        function(result) {
+            res.json({ id: result.insertId });
+            console.log({ id: result.insertId });
+        }
+    )
+});
+
+router.put("api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+    console.log("condition", condition);
+
+    burger.update({sleepy: req.body.sleepy}, condition, function(result) {
+        if(result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    })
+});
+
+router.delete("api/burgers/:id", function(req, res){
+    var condition = "id = " + req.params.id;
+    console.log("condition", condition);
+
+    burger.delete(condition, function(result) {
+        if(result.affectedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    })
+});
+
+module.exports = router;
